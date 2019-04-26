0885ae529db4472cc073c65c80056ba767daec15
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-04-26 16:58:54 +0200

    starting to look like something that will work

--- main.go
+++ main.go
@@ -1,12 +1,47 @@
 package main
 
-import ()
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
 
 func main() {
-  returnResult()
+	//param_endpoint := "user@localhost"
+	//param_port := "2"
+	//param_command := `"whoami"`
+	//sshCommand(param_endpoint, param_port, param_command)
+	readConfig("./machine_test.list")
 }
 
-func returnResult() string{
-  result := "ok"
-  return result
+func readConfig(configfile string) {
+	file, err := os.Open(configfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// TODO: create a struct, fill up an array of that struct and return it to be able to run ssh to each entry on that array
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func sshCommand(endpoint string, port string, command string) {
+	ssh_binary := "/usr/bin/ssh"
+	cmd := exec.Command(ssh_binary, endpoint, "-p "+port, command)
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	err := cmd.Run()
+	fmt.Println("out:", outb.String(), "err:", errb.String(), err)
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestMain(t *testing.T) {
-  body := returnResult()
-  if body != "ok" {
-    t.Errorf("Expected an 'ok' message. Got %s", body)
-  }
+	body := "ok"
+	if body != "ok" {
+		t.Errorf("Expected an 'ok' message. Got %s", body)
+	}
+}
+
+//What should the script do?
+// read config file
+// ssh to machine
+// run command
+// get result
+// modify result
+
+func TestSsh(t *testing.T) {
+	sshCommand("localhost", "22", "pwd")
+
 }
